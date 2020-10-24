d6ca2926b989f37424b1ada665a67254e751154c
Ore Adeleye <oreadeleye67@gmail.com>
2020-10-24 13:54:33 +0100

    back to server list anchor

--- resources/views/servers/show.blade.php
+++ resources/views/servers/show.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="app-header flex justify-between" style="color: {{ $server->color }}" x-data="{}">
-            <span>{{ $server->name }}</span>
-            <form @submit.prevent="leaveServer({{ route('server_members.remove', ['serverId' => $server->id]) }}, '{{ route('servers.index') }}')">
+        <div class="flex justify-between" x-data="{}">
+            <h2 class="app-header" style="color: {{ $server->color }}">{{ $server->name }}</h2>
+            <a href="{{ route('servers.index') }}" class="text-gray-600 hover:text-gray-800 mr-2 order-first">
+                <svg class="inline svg-icon"><use xlink:href="#icon-arrow-left2"></use></svg>
+                <span class="hidden md:inline">Back to server list</span>
+            </a>
+            <form @submit.prevent="leaveServer('{{ route('server_members.remove', ['serverId' => $server->id]) }}', '{{ route('servers.index') }}')">
                 <button type="submit" class="text-red-500 text-sm font-semibold" title="Leave server">
                     <svg class="inline svg-icon"><use xlink:href="#icon-exit"></use></svg>
                     <span class="hidden md:inline">Leave server</span>
                 </button>
             </form>
-        </h2>
+        </div>
     </x-slot>
 
     <div id="app" class="max-w-3xl mx-auto my-6 md:rounded shadow bg-gray-50">
