4784f89f37be0464350175dbf83a1f8b9c086520
Ore Adeleye <oreadeleye67@gmail.com>
2020-11-17 14:34:49 +0100

    FormattedMessage.js refactoring

--- resources/js/app/message/mixins/FormattedMessage.js
+++ resources/js/app/message/mixins/FormattedMessage.js
@@ -2,7 +2,7 @@ import marked from 'marked';
 
 export default {
     methods: {
-        marked: markdown => marked(markdown),
+        marked,
         highlight() {
             this.$refs.message.querySelectorAll('code').forEach(el => el.classList.add('microlight'));
             window.microlight.reset();
